368ef3abbd36e819974b3a546355a85529783982
danyal123 <d_hussain@live.com>
2022-04-26 12:53:07 -0700

    working commit

--- test/Drinks/Drinks/Models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Drinks(models.Model):
-    name = models.CharField(max_length=300)
-    description = models.CharField(max_length=300)
--- test/Drinks/Drinks/admin.py
+++
@@ -1,6 +0,0 @@
-import imp
-from multiprocessing.spawn import import_main_path
-from django.contrib import admin
-from .Models import Drinks
-
-admin.site.register(Drinks)
